a216fc8091c1a7d63775c93b36dba51fb7b6c5fe
Korto11 <corentin.soler77@gmail.com>
2023-07-17 16:32:00 +0200

    js

--- 5_Exercices_En_Cours_Formation/Javascript/projet_1/app.js
+++ 5_Exercices_En_Cours_Formation/Javascript/projet_1/app.js
@@ -1,18 +1,86 @@
-console.log('Bienvenue dans Javascript');
-let string = "bonjour";
-let int = 42;
-let float = 420.69;
-let array = [4,6,9];
-let object = {nom:"Jotaro",prénom:"Kujo",age:17};
-function helloworld(text) {
-    console.log(text);
-}
-console.log(string);
-console.log(int);
-console.log(float);
-console.log(array);
-console.log(object);
-let hello = helloworld("Hello World");
+
+// function pizza () {
+//     let pizzaQuantité;
+//     let pizzaTaille;
+//     let pizzaType;
+//     return {"Qté":pizzaQuantité,"taille":pizzaTaille,"type":pizzaType};
+// }
+// let pizzaOrder= [];
+// pizzaOrder.push(pizza);
+
+// let pizza = {
+// quantité : 2,
+// taille : "moyenne",
+// type : "regina"
+// }
+// let SumUpOrderPhrase = `Voici le récapitulatif de votre commande :
+// Pizza : ${pizza.type}. 
+// Taille : ${pizza.taille}.
+// Quantité : ${pizza.quantité}.
+// Merci d'avoir commandé chez "La Pizzeria Raffinata"
+// Qu'est ce que qu'une pizza dit à une autre pizza qui lui fait de l'ombre ?
+// "Arrête de me trancher la route!"`
+// console.log(SumUpOrderPhrase);
+
+
+
+// let bonjour = 'Bonjour';
+// let unMessage = "Bienvenue";
+// let welcome = `Bienvenue`;
+// console.log(bonjour,unMessage);
+// let unTexte = "Bonjour \"MR Gingle\"";
+// let unTxt = 'Aujourd\'hui';
+// console.log(unTexte,unTxt);
+// let concatenation = bonjour + " et " + unMessage +', il fait beau temps' + unTxt;
+// console.log(concatenation);
+// let uneTemplateString = `Hello ! ${bonjour} et ${unMessage}
+// on retourne à la "ligne" plus besoin des 'antislash'`;
+// console.log(uneTemplateString);
+
+// //* Exemple la "Pizzeria Rafinata"
+// let product = 'Pizza Hawaienne';
+// let nomUser = 'Jean Jack';
+// let date = '12-12-2012';
+// let nomPizzeria = `"La Pizzeria Rafinata"`
+// let SumUpOrderPhrase = `le ${date} Bonjour cher ${nomUser}, merci d'avoir passé commande chez ${nomPizzeria}, votre pizza :  ${product}, arrive sous 30 minutes
+
+
+// Qu'est-ce qu'une pizza dit à une autre pizza qui lui demandait 
+// des conseils ? "Suis ta pate et tout ira bien"`;
+
+// console.log(SumUpOrderPhrase);
+
+// let name = "Jacky";
+// let age = 24;
+// let passion = ["couture","peinture"];
+// let tabUser = [name,age,passion];
+// console.log (tabUser);
+// console.log(tabUser[2]);
+// console.log(tabUser[2][1]);
+
+let leNom = "Soler";
+let lePrénom = "Corentin";
+let laPhrase=[];
+laPhrase.push(leNom,lePrénom,leNom[0]+lePrénom[0]);
+console.log(laPhrase);
+
+
+
+// console.log('Bienvenue dans Javascript');
+// let string = "bonjour";
+// let int = 42;
+// let float = 420.69;
+// let array = [4,6,9];
+// let object = {nom:"Jotaro",prénom:"Kujo",age:17};
+// function helloworld(text) {
+//     console.log(text);
+// }
+// console.log(string);
+// console.log(int);
+// console.log(float);
+// console.log(array);
+// console.log(object);
+// let hello = helloworld("Hello World");
 /**
  * **************************************
  * 1-SETUP : DEFER
